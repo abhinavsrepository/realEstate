aec0d366b7d2261d955c61039a37ece82891b707
Abhinav Singh <abhi2510979b@gmail.com>
2025-06-19 23:43:03 +0530

    password encrption added

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,12 @@
-export const register =(req,res)=>{
-    console.log(req.body)
+import bcrypt from "bcrypt"
+
+export const register = async(req,res)=>{
+const{username,email,password} =req.body;
+
+// hash the password
+// create a new user and save to do
+const hashedPassword = await bcrypt.hash(password,10)
+console.log(hashedPassword)
 }
 export const login =(req,res)=>{
 

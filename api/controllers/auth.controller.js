f43bebd28ee3d4252daa1a0434d7bfc54797b017
Abhinav Singh <abhi2510979b@gmail.com>
2025-06-20 23:51:16 +0530

    initial commit

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,12 +1,29 @@
 import bcrypt from "bcrypt"
+import prisma from "../lib/prisma.js";
 
 export const register = async(req,res)=>{
 const{username,email,password} =req.body;
+try{
+
+
 
 // hash the password
 // create a new user and save to do
 const hashedPassword = await bcrypt.hash(password,10)
-console.log(hashedPassword)
+const newUser = await prisma.user.create({
+    data:{
+        username,
+        email,
+        password:hashedPassword,
+
+    }
+})
+console.log(newUser);
+res.status(201).json({message:"user created successfully"})
+}catch(err){
+    res.status(500).json({message:"failed to create user"})
+}
+
 }
 export const login =(req,res)=>{
 

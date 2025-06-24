17f82973a65f736ae98c636e8b194e6126627fc8
Abhinav Singh <abhi2510979b@gmail.com>
2025-06-25 02:57:41 +0530

    client initialised

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.scss";
-import { AuthContextProvider } from "./context/AuthContext.jsx";
-import { SocketContextProvider } from "./context/SocketContext.jsx";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthContextProvider>
-      <SocketContextProvider>
-        <App />
-      </SocketContextProvider>
-    </AuthContextProvider>
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
